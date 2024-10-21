4718ee7b6a7cf84344d306f3e74af724110607a1
Gavin-Persad <gavinapersad@gmail.com>
2024-10-21 12:37:11 +0100

    tasks complete

---
+++ workHere/main.js
@@ -0,0 +1,31 @@
+console.log('Hello World');
+
+//task 1
+
+let myNumber = 80;
+
+if (myNumber === 5) {
+	console.log('My Favorite Number is 5 too');
+}
+
+//task 2
+
+let answer = prompt('What is your name?');
+
+if (answer === 'Ben') {
+	console.log('That is my name too');
+} else {
+	console.log(`${answer}?! that is a silly name...`);
+}
+
+//task 3
+
+let weatherType = 'raining';
+
+if (weatherType === 'raining') {
+	console.log('take an umbrella!');
+} else if (weatherType === 'sunny') {
+	console.log("don/'t forget suncream!");
+} else {
+	console.log('must be a normal, grey, non descriptive day!');
+}
